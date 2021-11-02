74a45b86ce3fd30b0cdb73a8998bba1dc97b7c03
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2021-11-02 11:50:10 -0400

    iniciado e limpado projeto

---
+++ backend/src/test/java/com/wandersontimoteo/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wandersontimoteo.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
