e1a03009f1b7b0bb35b6fedbc97d9047dd214838
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2021-11-06 18:53:55 -0400

    Group by search

--- backend/src/main/java/com/wandersontimoteo/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/wandersontimoteo/dsvendas/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.wandersontimoteo.dsvendas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.wandersontimoteo.dsvendas.dto.SaleDTO;
+import com.wandersontimoteo.dsvendas.dto.SaleSuccessDTO;
+import com.wandersontimoteo.dsvendas.dto.SaleSumDTO;
 import com.wandersontimoteo.dsvendas.entities.Sale;
 import com.wandersontimoteo.dsvendas.repositories.SaleRepository;
 import com.wandersontimoteo.dsvendas.repositories.SellerRepository;
@@ -27,4 +31,15 @@ public class SaleService {
 		return result.map(x -> new SaleDTO(x));
 	}
 
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+		
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
+		
+	}
 }
